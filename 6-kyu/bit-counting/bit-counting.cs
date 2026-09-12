c9f19b9c34e93d7580dc90b028a88599356ac97f
chmny('-') <6dariaf0m@gmail.com>
2026-09-12 21:20:36 +0300

    Add solution - CodeHub

---
+++ 6-kyu/bit-counting/bit-counting.cs
@@ -0,0 +1,15 @@
+using System;
+​
+public class Kata
+{
+    public static int CountBits(int n)
+    {
+      int count = 0;
+      while(n>0)
+      {
+        if(n % 2 == 1) count+=1;
+        n = n / 2;
+      }
+      return count;
+    }
+}
